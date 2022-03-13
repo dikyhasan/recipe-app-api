1080ea04ad442167710f0b3bba81750e364ccbd5
dikyhasan <dikyhasan155@gmail.com>
2022-03-13 22:41:11 +0700

    Add Create User Apps

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """
+    Create a new user in the system
+    """
+
+    serializer_class = UserSerializer
